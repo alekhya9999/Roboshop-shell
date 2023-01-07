a44d8e9c05da8b8aae2d2edd540d6f25dbbf6931
alekhya9999 <alekya02@gmail.com>
2023-01-07 15:27:29 +0000

    common

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,8 @@
-
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
-
-status_check() {
-if [ $? -eq 0 ]; then
-echo SUCCESS
-else
-echo Failure
-echo Please Refer file log - ${LOG}
-exit
-fi
-    
-}
+source common.sh
 
 echo -e "\e[35m configuring nodejs repos\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${LOG}
-status_check()
+status_check
 
 
 echo -e "\e[35m Install nodejs\e[0m"
@@ -23,56 +10,56 @@ yum install nodejs -y  &>>${LOG}
 
 echo -e "\e[35m Add Application User\e[0m"
 useradd roboshop  &>>${LOG}
-status_check()
+status_check
 
 
 
 
 mkdir -p /app  &>>${LOG}
-status_check()
+status_check
 
 echo -e "\e[35m Downloading App Content\e[0m"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>${LOG}
-status_check()
+status_check
 
 echo -e "\e[35m CleanUp old Content\e[0m"
 rm -rf /app/* &>>${LOG}
-status_check()
+status_check
 
 echo -e "\e[35m Extracting App Content\e[0m"
 cd /app
 unzip /tmp/catalogue.zip  &>>${LOG}
 npm install   &>>${LOG}
-status_check()
+status_check
 
 
 echo -e "\e[35m configuring Catalogue Service Files\e[0m"
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service  &>>${LOG}
-status_check()
+status_check
 
 echo -e "\e[35m Reload systemD\e[0m"
 systemctl daemon-reload  &>>${LOG}
-status_check()
+status_check
 
 echo -e "\e[35m Enable catalogue service\e[0m"
 systemctl enable catalogue   &>>${LOG}
-status_check()
+status_check
 
 echo -e "\e[35m Start catalogue service\e[0m"
 systemctl start catalogue  &>>${LOG}
-status_check()
+status_check
 
 
 echo -e "\e[35m configuring mongodb repo\e[0m"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>${LOG}
-status_check()
+status_check
 
 
 echo -e "\e[35m Install mongo client\e[0m"
 yum install mongodb-org-shell -y  &>>${LOG}
-status_check()
+status_check
 
 
 echo -e "\e[35m load Schema\e[0m"
 mongo --host mongodb-dev.ambatis.online </app/schema/catalogue.js  &>>${LOG}
-status_check()
+status_check
---
+++ common.sh
@@ -0,0 +1,15 @@
+
+script_location=$(pwd)
+LOG=/tmp/roboshop.log
+
+status_check() {
+if [ $? -eq 0 ]; then
+echo SUCCESS
+else
+echo Failure
+echo Please Refer file log - ${LOG}
+exit
+fi
+    
+}
+
