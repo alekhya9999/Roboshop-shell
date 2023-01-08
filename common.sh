359ef0e1bf1cd88980968c45651c0d9537dd5487
alekhya9999 <alekya02@gmail.com>
2023-01-08 10:42:34 +0000

    all

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@ LOG=/tmp/roboshop.log
 
 status_check() {
 if [ $? -eq 0 ]; then
-echo SUCCESS
+ echo -e "\e[1;32mSUCCESS\e[0m"
 else
 echo Failure
 echo Please Refer file log - ${LOG}
@@ -16,77 +16,148 @@ print_head () {
     echo -e "\e[1m $1 \e[0m"
 }
 
-NODEJS () {
-    
-print_head "configuring node js repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${LOG}
-status_check
 
+APP_PREREQ() {
 
-print_head "Install nodejs"
-yum install nodejs -y  &>>${LOG}
+  print_head "Add Application User"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${LOG}
+  fi
+  status_check
 
-print_head "Add Application User"
-id roboshop &>>${LOG}
+  mkdir -p /app &>>${LOG}
 
- if [ $? -ne 0 ]; then
-useradd roboshop  &>>${LOG}
-fi
-status_check
+  print_head "Downloading App content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+  status_check
 
+  print_head "Cleanup Old Content"
+  rm -rf /app/* &>>${LOG}
+  status_check
 
+  print_head "Extracting App Content"
+  cd /app
+  unzip /tmp/${component}.zip &>>${LOG}
+  status_check
 
-
-mkdir -p /app  &>>${LOG}
-status_check
-
- print_head "Downloading App Content"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>${LOG}
-status_check
-
-print_head "CleanUp old Content"
-rm -rf /app/* &>>${LOG}
-status_check
-
-print_head "Extracting App Content"
-cd /app
-unzip /tmp/${component}.zip  &>>${LOG}
-npm install   &>>${LOG}
-status_check
-
-
-print_head " configuring component Service Files"
-cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service  &>>${LOG}
-status_check
-
-print_head " Reload systemD"
-systemctl daemon-reload  &>>${LOG}
-status_check
-
-print_head " Enable ${component} servic"
-systemctl enable ${component}   &>>${LOG}
-status_check
-
-print_head " Start ${component} servic"
-systemctl start ${component}  &>>${LOG}
-status_check
-
-
-
-if [ ${schema_load} == "true" ]; then
-print_head " configuring mongodb repo"
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>${LOG}
-status_check
-
-
-print_head " Install mongo client"
-yum install mongodb-org-shell -y  &>>${LOG}
-status_check
-
-
-print_head " load Schema"
-mongo --host mongodb-dev.ambatis.online </app/schema/${component}.js  &>>${LOG}
-status_check
-fi
-    
 }
+
+
+
+SYSTEMD_SETUP() {
+ 
+  print_head "Configuring ${component} Service File"
+  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  status_check
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "Enable ${component} Service "
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "Start ${component} service "
+  systemctl start ${component} &>>${LOG}
+  status_check
+
+}
+
+
+
+LOAD_SCHEMA() {
+  if [ ${schema_load} == "true" ]; then
+
+    if [ ${schema_type} == "mongo"  ]; then
+      print_head "Configuring Mongo Repo "
+      cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+      status_check
+
+      print_head "Install Mongo Client"
+      yum install mongodb-org-shell -y &>>${LOG}
+      status_check
+
+      print_head "Load Schema"
+      mongo --host mongodb-dev.ambatis.online </app/schema/${component}.js &>>${LOG}
+      status_check
+    fi
+
+    if [ ${schema_type} == "mysql"  ]; then
+
+      print_head "Install MySQL Client"
+      yum install mysql -y &>>${LOG}
+      status_check
+
+      print_head "Load Schema"
+      mysql -h mysql-dev.ambatis.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+      status_check
+    fi
+
+  fi
+}
+
+
+NODEJS() {
+  print_head "Configuring NodeJS Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  APP_PREREQ
+
+  print_head "Installing NodeJS Dependencies"
+  cd /app &>>${LOG}
+  npm install &>>${LOG}
+  status_check
+
+  SYSTEMD_SETUP
+
+  LOAD_SCHEMA
+}
+
+
+MAVEN() {
+
+  print_head "Install Maven"
+  yum install maven -y &>>${LOG}
+  status_check
+
+  APP_PREREQ
+
+  print_head "Build a package"
+  mvn clean package  &>>${LOG}
+  status_check
+
+  print_head "Copy App file to App Location"
+  mv target/${component}-1.0.jar ${component}.jar
+  status_check
+
+  SYSTEMD_SETUP
+
+  LOAD_SCHEMA
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
